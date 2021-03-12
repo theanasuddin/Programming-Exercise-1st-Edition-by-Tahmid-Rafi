35d4e45e95edad3bbfe23fae588f44e0e617b55e
Anas Uddin <anasuddin44@gmail.com>
2021-03-12 21:14:21 +0600

    Create Problem1.java

---
+++ অধ্যায় এক - ডেটা টাইপ, ইনপুট ও আউটপুট/সাধারণ ইনপুট ও আউটপুট/অনুশীলনী 1.1/Problem1.java
@@ -0,0 +1,11 @@
+package programmingExercise;
+
+public class Problem1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("Saturday\nSunday\nMonday\nTuesday\nWednesday\nThursday\nFriday\n");
+
+	}
+
+}
