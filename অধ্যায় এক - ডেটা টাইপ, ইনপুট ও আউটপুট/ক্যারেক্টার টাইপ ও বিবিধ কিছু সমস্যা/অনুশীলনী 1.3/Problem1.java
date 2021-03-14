1191c462acb6630d493f5547f092c16248fab33d
Anas Uddin <anasuddin44@gmail.com>
2021-03-14 21:00:29 +0600

    Create Problem1.java

---
+++ অধ্যায় এক - ডেটা টাইপ, ইনপুট ও আউটপুট/ক্যারেক্টার টাইপ ও বিবিধ কিছু সমস্যা/অনুশীলনী 1.3/Problem1.java
@@ -0,0 +1,18 @@
+package programmingExercise;
+
+import java.util.Scanner;
+
+public class Problem1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int ASCII;
+
+		ASCII = scan.nextInt();
+		scan.close();
+
+		System.out.printf("%c\n", ASCII);
+	}
+
+}
