125973f2e9866da65f407e4658ee60f8066f1ada
Anas Uddin <anasuddin44@gmail.com>
2021-03-23 22:26:20 +0600

    Update Problem1.java

--- অধ্যায় দুই - কন্ডিশনাল লজিক/if-else if-else/Problem1.java
+++ অধ্যায় দুই - কন্ডিশনাল লজিক/if-else if-else/Problem1.java
@@ -2,7 +2,7 @@ package programmingExercise;
 
 import java.util.Scanner;
 
-public class Problem1_1 {
+public class Problem1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
