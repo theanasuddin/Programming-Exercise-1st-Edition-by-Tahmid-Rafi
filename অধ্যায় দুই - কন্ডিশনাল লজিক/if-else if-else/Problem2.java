1d36f1bbc4b60c462b73dac5e9954fc19126a199
Anas Uddin <anasuddin44@gmail.com>
2021-03-23 22:25:33 +0600

    Create Problem2.java

---
+++ অধ্যায় দুই - কন্ডিশনাল লজিক/if-else if-else/Problem2.java
@@ -0,0 +1,30 @@
+package programmingExercise;
+
+import java.util.Scanner;
+
+public class Problem2_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int bus, trawler, auto_rickshaw, money;
+
+		bus = scan.nextInt();
+		trawler = scan.nextInt();
+		auto_rickshaw = scan.nextInt();
+		money = scan.nextInt();
+		scan.close();
+
+		if (money >= auto_rickshaw) {
+			System.out.printf("Autorickshaw.\n");
+		} else if (money >= trawler) {
+			System.out.printf("Trawler.\n");
+		} else if (money >= bus) {
+			System.out.printf("Bus.\n");
+		} else {
+			System.out.printf("Walk.\n");
+		}
+
+	}
+
+}
