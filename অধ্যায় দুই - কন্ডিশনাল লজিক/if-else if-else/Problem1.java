1616dcd233744c2a8e85029856ac51e92aefc430
Anas Uddin <anasuddin44@gmail.com>
2021-03-23 22:24:55 +0600

    Create Problem1.java

---
+++ অধ্যায় দুই - কন্ডিশনাল লজিক/if-else if-else/Problem1.java
@@ -0,0 +1,26 @@
+package programmingExercise;
+
+import java.util.Scanner;
+
+public class Problem1_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int a, b;
+
+		a = scan.nextInt();
+		b = scan.nextInt();
+		scan.close();
+
+		if (a > b) {
+			System.out.printf("a is greater than b.\n");
+		} else if (b > a) {
+			System.out.printf("b is greater than a.\n");
+		} else {
+			System.out.printf("a and b are equal.\n");
+		}
+
+	}
+
+}
