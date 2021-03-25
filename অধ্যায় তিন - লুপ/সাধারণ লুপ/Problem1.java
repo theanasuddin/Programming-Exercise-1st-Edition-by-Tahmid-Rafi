c8bf3df5e7efb792d2042fc3daf2cf0e66708302
Anas Uddin <anasuddin44@gmail.com>
2021-03-25 11:25:55 +0600

    Create Problem1.java

---
+++ অধ্যায় তিন - লুপ/সাধারণ লুপ/Problem1.java
@@ -0,0 +1,11 @@
+package programmingExercise;
+
+public class Problem1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 97; i <= 122; i++) {
+			System.out.printf("%c ", i);
+		}
+	}
+}
