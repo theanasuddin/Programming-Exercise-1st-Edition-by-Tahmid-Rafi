2c37989995423c80d8e22d99554003bce4d54855
Anas Uddin <anasuddin44@gmail.com>
2021-03-31 11:58:32 +0600

    Create Problem1.java

---
+++ অধ্যায় তিন - লুপ/লুপ ও কন্ডিশনাল লজিক/অনুশীলনী 3.2/Problem1.java
@@ -0,0 +1,15 @@
+package programmingExercise;
+
+public class Problem1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i <= 1001; i++) {
+			System.out.printf("%4d ", i);
+
+			if (i % 7 == 0) {
+				System.out.printf("\n");
+			}
+		}
+	}
+}
